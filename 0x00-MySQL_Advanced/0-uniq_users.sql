06622b87d8ad9ee9c137589b5cbb4b1462aca360
amineNouabi <aminenouabi9@gmail.com>
2024-10-18 16:24:44 +0100

    users table

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -2,10 +2,9 @@
 -- users: id, name, email
 -- id is the primary key
 -- email is unique
--- name and email are not null
 
 CREATE TABLE IF NOT EXISTS users (
 	id INT PRIMARY KEY AUTO_INCREMENT,
-	name VARCHAR(255) NOT NULL,
-	email VARCHAR(255) NOT NULL UNIQUE
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255)
 );
