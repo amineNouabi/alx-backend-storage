8de9bb56c3bf5a1392d32700c8a32ce1501c3849
amineNouabi <aminenouabi9@gmail.com>
2024-10-19 21:09:22 +0100

    glam rock bands

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -2,5 +2,5 @@
 -- ranked by their longevity.
 SELECT band_name, (2022 - formed) AS lifespan
 FROM metal_bands
-WHERE style LIKE '%Glam rock%'
+WHERE LOWER(style) LIKE '%glam rock%'
 ORDER BY lifespan DESC;
