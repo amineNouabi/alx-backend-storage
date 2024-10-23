741932299764ba666940e9b4d60db5dcd539bf65
amineNouabi <aminenouabi9@gmail.com>
2024-10-23 11:41:05 +0100

    Redis Cache Class

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -18,8 +18,8 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self: "Cache", data: Union[str, bytes, int, float]) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """Stores the input data in the cache"""
-        key = uuid.uuid4()
-        self._redis.append(key, data)
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
         return key
