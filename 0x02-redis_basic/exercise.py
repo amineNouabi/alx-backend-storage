a27142af789a9981fa250a8d661f671b2da54b29
amineNouabi <aminenouabi9@gmail.com>
2024-10-23 12:01:54 +0100

    Redis Cache Class

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -7,7 +7,7 @@ Module defining the Cache class
 
 import redis
 import uuid
-from typing import Union
+from typing import Union, Optional, Callable
 
 
 class Cache:
@@ -22,4 +22,20 @@ class Cache:
         """Stores the input data in the cache"""
         key = str(uuid.uuid4())
         self._redis.set(key, data)
+        self._redis.get
         return key
+
+    def get(
+            self, key: str, fn: Optional[Callable] = None
+    ) -> Union[str, bytes, int, float]:
+        """Gets the data stored in the cache"""
+        data = self._redis.get(key)
+        return fn(data) if fn else data
+
+    def get_str(self, key: str) -> str:
+        """Gets the data stored in the cache as string"""
+        return self.get(key, lambda x: x.decode('utf-8'))
+
+    def get_int(self, key: str) -> int:
+        """Gets the data stored in the cache as int"""
+        return self.get(key, int)
